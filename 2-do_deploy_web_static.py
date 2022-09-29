c964f8c7c98744c8d7cdd7feb4ae47ffef7080cd
Hizaram <promisevector@gmail.com>
2022-09-29 22:24:49 +0100

    Removed the unnecessary function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,14 @@
 #!/usr/bin/python3
-""" Script that distributes an archive to web_servers, using do_deploy
+"""Script that distributes an archive to web_servers, using do_deploy
 """
 from fabric.api import local, env, run, put, sudo
-from fabric import operations
-from datetime import datetime
 
 
 env.hosts = ['3.81.28.185', '3.238.90.118']
 
 
-def do_pack():
-    """ The do_pack function that generates the .tgz archive"""
-    # Create versions dir if it doesn't exist
-    operations.local("mkdir -p versions")
-
-    # format of file to be made:
-    # web_static_<year><month><day><hour><minute><second>.tgz
-    format1 = "%Y%m%d%H%M%S"
-    file_time = datetime.utcnow().strftime(format1)
-    filename = "web_static_"
-    filename += file_time
-    filename += ".tgz"
-
-    # Create the .tgz archive
-    result = operations.local(
-            "tar -cvzf 'versions/{}' web_static".
-            format(filename))
-
-    # If archive was made, return. Otherwise , return None
-    if result is not None:
-        return result
-    else:
-        return None
-
-
 def do_deploy(archive_path):
-    """ Function that deploys archive to web_servers"""
+    """Function that deploys archive to web_servers"""
     if archive_path:
         archive_file = archive_path.split("/")[1]
         archive_dir = archive_file.split(".")[0]
