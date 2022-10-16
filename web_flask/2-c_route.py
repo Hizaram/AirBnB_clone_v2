592bda784e956e9ff360cc6efc5a1b764b85c1ec
Hizaram <promisevector@gmail.com>
2022-10-16 19:40:55 +0100

    Added the script that displays `C <text> when navigated to `/C/<text>`

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Python module that returns string when navigating to root directory"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Return the welcome message"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Return HBNB when navigated to"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_text(text):
+    """Route /c/<text> returns C message"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
